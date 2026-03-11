ae51a1c31d07548ae1cb5b6fa3790ecfd244d68f
rain <lcmm5201314@gmail.com>
2026-03-11 18:30:40 +0800

    feat: test redis

--- redistest/chat02/parse.go
+++ redistest/chat02/parse.go
@@ -1,5 +1,6 @@
 package main
 
+import "C"
 import (
 	"bufio"
 	"fmt"
@@ -12,64 +13,49 @@ import (
 
 func Handel(c net.Conn) error {
 
+	defer func() {
+		if err := c.Close(); err != nil {
+			log.Println("CloseError: ", err)
+		}
+	}()
+
 	reader := bufio.NewReader(c)
-	writer := bufio.NewWriter(c)
 	line, err := reader.ReadBytes('\n')
 	if err != nil {
 		return err
 	}
 
 	if len(line) <= 2 {
-		_, _ = writer.Write(NewUnknownCMDReply().ToBytes())
+		_, _ = c.Write(NewUnknownCMDReply().ToBytes())
 		return fmt.Errorf("invalid cmd %s", line)
 	}
 
 	if line[0] != '*' {
-		_, _ = writer.Write(NewUnknownCMDReply().ToBytes())
+		_, _ = c.Write(NewUnknownCMDReply().ToBytes())
 		return fmt.Errorf("invalid prefix %s", line)
 	}
 
 	reply, err := NewArrayReply(line[1:], reader)
 	if err != nil {
-		_, _ = writer.Write(NewParseErrReply().ToBytes())
+		_, _ = c.Write(NewParseErrReply().ToBytes())
 		return err
 	}
 
 	sevName, err := Auth(reply.(*ArrayReply))
 	if err != nil {
-		_, _ = writer.Write(NewErrReply([]byte(err.Error())).ToBytes())
+		_, _ = c.Write(NewErrReply([]byte(err.Error())).ToBytes())
 		return err
 	}
-
-	fmt.Println("--------- 111 ")
 
 	server, err := GetRedisServer(sevName)
 	if err != nil {
-		_, _ = writer.Write(NewErrReply([]byte(err.Error())).ToBytes())
+		_, _ = c.Write(NewErrReply([]byte(err.Error())).ToBytes())
 		return err
 	}
 
-	fmt.Println("--------- 222 ")
-
-	var wg sync.WaitGroup
-	wg.Go(func() {
-		if _, err := io.Copy(server, reader); err != nil {
-			log.Println("io.Copy: ", err)
-		}
-	})
-	wg.Go(func() {
-		if _, err := io.Copy(writer, server); err != nil {
-			log.Println("io.Copy: ", err)
-		}
-	})
-
-	fmt.Println("--------- 333 ")
-
 	// 回复认证成功
-	resp := NewOkReply().ToBytes()
-	fmt.Println("===> resp ", string(resp))
-	_, _ = writer.Write(resp)
-	wg.Wait()
+	_, _ = c.Write(NewOkReply().ToBytes())
+	Tcp(c, server)
 	return nil
 }
 
@@ -93,4 +79,19 @@ func Auth(reply *ArrayReply) (string, error) {
 		return "", fmt.Errorf("Auth: params is not 2! ")
 	}
 	return string(reply.data[1]), nil
+}
+
+func Tcp(cli, sev net.Conn) {
+	var wg sync.WaitGroup
+	wg.Go(func() {
+		if _, err := io.Copy(cli, sev); err != nil {
+			log.Println("io.Copy: ", err)
+		}
+	})
+	wg.Go(func() {
+		if _, err := io.Copy(sev, cli); err != nil {
+			log.Println("io.Copy: ", err)
+		}
+	})
+	wg.Wait()
 }
